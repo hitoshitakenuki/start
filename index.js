f24f166c4f2b75a092fc65cb38c3493aeca94bb5
hitoshitakenuki <reportfrombamboo@gmail.com>
2021-10-17 10:24:19 +0900

    database

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const path = require('path')
+var {Client} = require('pg');
 const PORT = process.env.PORT || 5000
+
+var client = new Client({
+  user: 'bxdhrqzeozmuws', // DB のユーザー名を指定
+  host: 'ec2-18-207-72-235.compute-1.amazonaws.com',
+  database: 'd2umj76u4q9ods',
+  password: '021af8e3cc3e2d38f94982afedd7741c6214e93fbb09d0400e1d8373120b233e', // DB のパスワードを指定
+  post: 5432
+})
+
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+  }else{
+  console.log('success');
+  }
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +25,5 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
